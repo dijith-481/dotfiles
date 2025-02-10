d72dec7cfb7978a1774fe3ffc558bf74131bb46d
dijith-481 <dijithdinesh@protonmail.com>
2025-02-10 20:32:55 +0530

    ytm layout

--- hypr/scripts/yt-music.sh
+++ hypr/scripts/yt-music.sh
@@ -1,10 +1,19 @@
 #!/bin/bash 
-if hyprctl activeworkspace | grep -q "workspace ID 11" ; then
-	    brave --app=https://music.youtube.com
-else
 	if  hyprctl clients | grep -q "music.youtube.com" ; then
 		hyprctl dispatch workspace 11
+
+		if  !(hyprctl clients | grep -q "cava0") ; then
+		kitty --class cava0 -e cava &bg
+		fi
+		if  !(hyprctl clients | grep -q "cava1") ; then
+		kitty --class cava1 -e cava &bg
+		fi
+		if  !(hyprctl clients | grep -q "cava2") ; then
+		kitty --class cava2 -e cava &bg
+		fi
 	else
-	    brave --app=https://music.youtube.com
+	    brave --app=https://music.youtube.com 
+	    kitty --class cava0 -e cava &bg
+	    kitty --class cava1 -e cava &bg
+	    kitty --class cava2 -e cava &bg
 	fi
-fi
