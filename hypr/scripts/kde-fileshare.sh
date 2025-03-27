1036bd9cc5709631c9d36ba47c37f3a8db03cc59
dijith-481 <dijithdinesh@protonmail.com>
2025-03-27 19:22:07 +0530

    fix bugs windowrulev2

--- hypr/scripts/kde-fileshare.sh
+++ hypr/scripts/kde-fileshare.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
-selected=$(clipse -p)
-if [[ ! -f "$selected" && ! "$selected" =~ ^(http|https|ftp):// ]]; then
-  tempfile=$(mktemp)
-  kitty sh -c "fzf > '$tempfile'" || exit 1
-  selected=$(cat "$tempfile")
-  rm -f "$tempfile"
-fi
-
-if [[ -n "$selected" ]]; then
-  kdeconnect-cli -n "Galaxy A52s 5G" --share "$selected"
+header=$(clipse -p | head -c 8 | xxd -p)
+if [[ "$header" == "89504e470d0a1a0a" ]]; then
+  tempfile=$(mktemp --suffix .png)
+  clipse -p >"$tempfile"
+  kdeconnect-cli -n "Galaxy A52s 5G" --share "$tempfile"
+else
+  selected=$(clipse -p)
+  if [[ ! -f "$selected" && ! "$selected" =~ ^(http|https|ftp):// ]]; then
+    tempfile=$(mktemp)
+    kitty sh -c "fzf > '$tempfile'" || exit 1
+    selected=$(cat "$tempfile")
+    rm -f "$tempfile"
+  fi
+  if [[ -n "$selected" ]]; then
+    kdeconnect-cli -n "Galaxy A52s 5G" --share "$selected"
+  fi
 fi
--- hypr/scripts/yt-music.sh
+++ hypr/scripts/yt-music.sh
@@ -2,13 +2,13 @@
 if hyprctl clients | grep -q "music.youtube.com"; then
   hyprctl dispatch workspace 11
 
-  if !(hyprctl clients | grep -q "cava0"); then
+  if ! (hyprctl clients | grep -q "cava0"); then
     kitty -c ~/.config/kitty/cava.conf --class cava0 -e cava &
   fi
-  if !(hyprctl clients | grep -q "cava1"); then
+  if ! (hyprctl clients | grep -q "cava1"); then
     kitty -c ~/.config/kitty/cava.conf --class cava1 -e cava &
   fi
-  if !(hyprctl clients | grep -q "cava2"); then
+  if ! (hyprctl clients | grep -q "cava2"); then
     kitty -c ~/.config/kitty/cava.conf --class cava2 -e cava &
   fi
 else
