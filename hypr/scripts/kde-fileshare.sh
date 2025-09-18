e976b602cdc506bb625a88336003c472c7ce45c2
dijith-481 <dijithdinesh@protonmail.com>
2025-09-18 21:30:11 +0530

    file picker with fzf keybinding

--- hypr/scripts/kde-fileshare.sh
+++ hypr/scripts/kde-fileshare.sh
@@ -8,7 +8,7 @@ else
   selected=$(clipse -p)
   if [[ ! -f "$selected" && ! "$selected" =~ ^(http|https|ftp):// ]]; then
     tempfile=$(mktemp)
-    kitty sh -c "fzf > '$tempfile'" || exit 1
+    kitty -T="fzf-picker" sh -c "fzf > '$tempfile'" || exit 1
     selected=$(cat "$tempfile")
     rm -f "$tempfile"
   fi
